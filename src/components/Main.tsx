1ea4fcbfc9bdf216e178626c1ce7f54602eaf9fd
PranavPonni <pranavponni2003@gmail.com>
2026-03-03 22:34:27 +0900

    updated main page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,19 +9,51 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          {/* Replace this image with your own later (instructions below) */}
+          <img 
+            src="https://via.placeholder.com/300" 
+            alt="Pranav Ponni" 
+          />
         </div>
+
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a 
+              href="https://github.com/pranavponni" 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>
+
+            <a 
+              href="https://www.linkedin.com/in/pranav-ponni/" 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              <LinkedInIcon />
+            </a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+
+          <h1>Pranav Ponni</h1>
+          <p>Software Engineer | Full Stack Developer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a 
+              href="https://github.com/pranavponni" 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>
+
+            <a 
+              href="https://www.linkedin.com/in/pranav-ponni/" 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              <LinkedInIcon />
+            </a>
           </div>
         </div>
       </div>
